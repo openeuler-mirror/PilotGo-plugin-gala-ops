44c0452a1fb6411eab48bde2472531f46116c733
Wangjunqi123 <wangjunqi@kylinos.cn>
2023-06-26 17:22:31 +0800

    modify log info in getpkgversion()

--- server/agentmanager/command.go
+++ server/agentmanager/command.go
@@ -16,16 +16,16 @@ func GetPkgVersion(machines []*database.Agent, batch *common.Batch, pkgname stri
 	if err == nil {
 		for _, result := range cmdresults {
 			if result.RetCode == 1 && strings.Contains(result.Stdout, "is not installed") && result.Stderr == "" {
-				logger.Error("%s not installed happened when running getpackageversion: %s, %s, %s; ", pkgname, result.MachineUUID, result.MachineIP, result.Stderr)
+				logger.Error("%s not installed happened when running getpkgversion: %s, %s, %s; ", pkgname, result.MachineUUID, result.MachineIP, result.Stderr)
 				continue
 			} else if result.RetCode == 127 && result.Stdout == "" && strings.Contains(result.Stderr, "command not found") {
-				logger.Error("rpm not installed when running getpackageversion: %s, %s, %s", result.MachineUUID, result.MachineIP, result.Stderr)
+				logger.Error("rpm not installed when running getpkgversion: %s, %s, %s", result.MachineUUID, result.MachineIP, result.Stderr)
 				continue
 			} else if result.RetCode == 0 && len(result.Stdout) > 0 && result.Stderr == "" {
 				reader := strings.NewReader(result.Stdout)
 				v, err := utils.ReadInfo(reader, `^Version.*`)
 				if err != nil && len(v) != 0 {
-					logger.Error("failed to read RPM package version when getpackageversion: %s, %s, %s", result.MachineUUID, result.MachineIP, result.Stderr)
+					logger.Error("failed to read RPM package version when running getpkgversion: %s, %s, %s", result.MachineUUID, result.MachineIP, result.Stderr)
 					continue
 				}
 				for _, m := range machines {
@@ -43,7 +43,7 @@ func GetPkgVersion(machines []*database.Agent, batch *common.Batch, pkgname stri
 					}
 				}
 			} else {
-				logger.Error("failed to run command: rpm -qi %s in %s, %s, %s when getpackageversion", pkgname, result.MachineUUID, result.MachineIP, result.Stderr)
+				logger.Error("failed to run command: rpm -qi %s in %s, %s, %s when running getpkgversion", pkgname, result.MachineUUID, result.MachineIP, result.Stderr)
 				continue
 			}
 		}
